3f4df035ffe05ef6f06b98e6016cad9108c271fd
Markus Augusto <markus.augusto.sa@gmail.com>
2026-06-22 16:59:23 -0500

    TABLE Agenda completo

--- BrenoDB/BrenoDB.sql
+++ BrenoDB/BrenoDB.sql
@@ -17,3 +17,28 @@ CREATE TABLE Admin (
 
     FOREIGN KEY (ID_Usuario_FK) REFERENCES Usuario(ID_Usuário)
 );
+
+CREATE TABLE Aluno (
+    Matricula VARCHAR(20) UNIQUE NOT NULL,
+    Ano_Ingresso INT NOT NULL,
+    Curso VARCHAR(30) NOT NULL,
+    ID_Usuario_FK INT,
+
+    FOREIGN KEY (ID_Usuario_FK) REFERENCES Usuario(ID_Usuario)
+);
+
+CREATE TABLE Professor (
+    ID_Professor INT PRIMARY KEY,
+    Nome_Professor VARCHAR(50) NOT NULL,
+    Disciplina VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE Agenda (
+    ID_Agenda INT PRIMARY KEY,
+    Ano_Letivo INT UNIQUE NOT NULL,
+    Data_Agenda DATE,
+    Itens_Agenda VARCHAR(50),
+    ID_subAdmin_FK INT,
+
+    FOREIGN KEY (ID_subAdmin_FK) REFERENCES Usuario(ID_Usuario)
+);
