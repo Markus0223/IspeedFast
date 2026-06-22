20bf5cee28fdcc8db81cff9558cbdbd6f5d6f2be
Markus Augusto <markus.augusto.sa@gmail.com>
2026-06-22 17:51:15 -0500

    TABLE Sala completo

--- BrenoDB/BrenoDB.sql
+++ BrenoDB/BrenoDB.sql
@@ -10,10 +10,51 @@ CREATE TABLE Usuario (
     Tipo_Usuario VARCHAR NOT NULL
 );
 
-CREATE TABLE Admin (
+CREATE TABLE Administrador (
     Acesso VARCHAR(20) UNIQUE NOT NULL,
     Formacao VARCHAR(50) NOT NULL,
     ID_Usuario_FK INT,
 
-    FOREIGN KEY (ID_Usuario_FK) REFERENCES Usuario(ID_Usuário)
+    FOREIGN KEY (ID_Usuario_FK) REFERENCES Usuario(ID_Usuario)
+);
+
+CREATE TABLE Aluno (
+    Matricula VARCHAR(20) UNIQUE NOT NULL,
+    Ano_Ingresso INT NOT NULL,
+    Curso VARCHAR(30) NOT NULL,
+    ID_Usuario_FK INT,
+
+    FOREIGN KEY (ID_Usuario_FK) REFERENCES Usuario(ID_Usuario)
+);
+
+CREATE TABLE Professor (
+    ID_Professor INT PRIMARY KEY,
+    Nome_Professor VARCHAR(50) NOT NULL,
+    Disciplina VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE Agenda (
+    ID_Agenda INT PRIMARY KEY,
+    Ano_Letivo INT UNIQUE NOT NULL,
+    Data_Agenda DATE,
+    Itens_Agenda VARCHAR(50),
+    ID_subAdmin_FK INT,
+
+    FOREIGN KEY (ID_subAdmin_FK) REFERENCES Usuario(ID_Usuario)
+);
+
+CREATE TABLE Espaco (
+    ID_Espaco INT PRIMARY KEY,
+    Capacidade INT,
+    Status_Espaco BOOLEAN NOT NULL,
+    Tipo_Espaco VARCHAR(15) NOT NULL
+);
+
+CREATE TABLE Sala (
+    Numero_Sala VARCHAR(30) UNIQUE NOT NULL,
+    Qntd_Carteiras INT,
+    Projetor_Sala BOOLEAN,
+    ID_Espaco_FK INT,
+
+    FOREIGN KEY (ID_Espaco_FK) REFERENCES Espaco(ID_Espaco)
 );
